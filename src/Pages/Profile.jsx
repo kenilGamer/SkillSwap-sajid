5a417133b8eb4cc22effe58fe36608a21db65b90
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2023-12-27 13:41:41 +0530

    dd

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -7,14 +7,14 @@ const Profile = () => {
           <img src="/public/logo.jpg" width={50} alt="" />
           <h1 className="text-3xl font-extrabold py-0 text-center px-4 ">your profile</h1>
         </div>
-        <div className="w-[40%] my-10 h-36 m-12 flex">
+        <div className="w-[40%] my-10 h-36 m-3 flex">
           <div className="h-full columns-auto">
             <img className="w-full h-full object-cover rounded-md" src="/public/assets/Face 1.png" alt="" />
           </div>
           <div className=" mx-3 w-64 ">
            <div className="flex items-center gap-5">
-           <h1 className="text-xl font-bold">username </h1>
-            <span><MdEdit/></span>
+           <h1 className="text-2xl font-bold">username </h1>
+            <span className="mt-2"><MdEdit/></span>
            </div>
             <div> <h6 className="font-bold text-xs mb-3">[Designation]</h6></div>
             <div className="flex gap-9 md-3">
@@ -28,10 +28,10 @@ const Profile = () => {
             </div>
           </div>
         </div>
-        <div className="w-full h-52">
-          <div className="flex items-center gap-2 mb-4">
+        <div className="w-full h-44">
+          <div className="flex items-center gap-2 mb-2">
             <h1 className="text-2xl font-bold">SKILLS </h1>
-            <samp><MdEdit/></samp>
+            <samp ><MdEdit/></samp>
           </div>
           <div className="w-8/12 flex flex-wrap gap-3">
               <button className="border-solid px-4 py-2 border-b-slate-800 border-l-slate-500 border-t-yellow-500 border-r-yellow-300 rounded-3xl border-2"> SKILLS 1</button>
@@ -51,10 +51,24 @@ const Profile = () => {
          
           </div>
         </div>
-        <div className="">
-
+      <div className="flex flex-grow items-center justify-center self-stretch" >
+        <div className="flex flex-grow flex-col items-start justify-between self-stretch" >
+          <div className="flex h-6 w-56 items-center justify-center gap-x-2 leading-none text-black" >
+            <div className="flex w-48 items-center self-stretch">
+              Freely about yourself
+            </div>
+            <MdEdit />
+          </div>
+          <div className="flex items-center self-stretch leading-normal w-10/12 p-5 text-zinc-400" >
+            Write a few lines about yourself and your character. Tell us about your achievements, priorities and life experience. This way your profile will become more interesting and attract more attention.
+          </div>
         </div>
-     </div>
+      </div>
+      <div className="flex justify-end mr-16">
+        <button className="px-10 py-3 bg-sky-400 text-white rounded-full">Save & Continue</button>
+      </div>
+    </div>
+     
     </>
   )
 }
